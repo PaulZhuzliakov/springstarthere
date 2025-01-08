5b0063d75f2d18488282729324579c359a10724a
PaulZhuzliakov <paulparatrooper@gmail.com>
2025-01-09 00:37:53 +0300

    chapter 9

--- src/main/java/org/example/demo666/controller/MainController.java
+++ src/main/java/org/example/demo666/controller/MainController.java
@@ -1,15 +1,43 @@
 package org.example.demo666.controller;
 
+import org.example.demo666.component.LoggedUserManagementService;
+import org.example.demo666.service.LoginCountService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class MainController {
-    @RequestMapping("/home")
-    public String home(Model page) {
-        page.addAttribute("username", "Admin");
-        page.addAttribute("color", "red");
-        return "home0.html";
+
+    private final LoggedUserManagementService loggedUserManagementService;
+    private final LoginCountService loginCountService;
+
+    public MainController(LoggedUserManagementService loggedUserManagementService, LoginCountService loginCountService) {
+        this.loggedUserManagementService = loggedUserManagementService;
+        this.loginCountService = loginCountService;
     }
+
+    @GetMapping("/main")
+    public String main(
+            @RequestParam(required = false) String Logout,
+            Model model
+    ) {
+        if (Logout != null) {
+            loggedUserManagementService.setUsername(null);
+        }
+        String username = loggedUserManagementService.getUsername();
+        int count = loginCountService.getCount();
+
+        if (username == null) {
+            return "redirect:/";
+        } else {
+            model.addAttribute("username", username);
+            model.addAttribute("loginCount", count);
+            return "main.html";
+        }
+    }
+
+
+
 }
