9be09f72c56fe6e1a2ea39562b16c3e87efce316
PaulZhuzliakov <paulparatrooper@gmail.com>
2025-01-12 20:52:45 +0300

    chapter 12

---
+++ src/main/java/org/example/demo666/controller/PurchaseController.java
@@ -0,0 +1,28 @@
+package org.example.demo666.controller;
+
+import org.example.demo666.model.Purchase;
+import org.example.demo666.repository.PurchaseRepository;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/purchase")
+public class PurchaseController {
+    private final PurchaseRepository purchaseRepository;
+
+    public PurchaseController(PurchaseRepository purchaseRepository) {
+        this.purchaseRepository = purchaseRepository;
+    }
+
+    @PostMapping
+    public void storePurchase(@RequestBody Purchase purchase) {
+        purchaseRepository.storePurchase(purchase);
+    }
+
+    @GetMapping
+    public List<Purchase> findPurchases() {
+        return purchaseRepository.findAllPurchases();
+    }
+
+}
